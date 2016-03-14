2733c063c22c558d8e9ae43bfeb2492e4c085eb8
Aviendha <sopharnold.pr@gmail.com>
2016-03-14 19:04:48 +0000

    update

--- localtraveldiary.rb
+++ localtraveldiary.rb
@@ -10,8 +10,7 @@ post('/input') do
 	@memory = params[:memory]
 	@attraction = params[:attraction]
 	@food = params[:food]
-	"Thank you for sharing here's your holiday: I went on holiday to #{@destination} with #{@companions}. 
-	I really enjoyed trying the local #{@food} and visiting the #{@attraction}. 
-	I will never forget #{@memory}."
+	erb :input
+
 end
 
